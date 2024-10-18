5bee1c8b99c7cd49b3fd4d75b0b7b4aa8c657f7e
prosg <learner.in.dev@gmail.com>
2024-10-18 11:16:43 +0200

    Maj App.jsx, main.jsx, main.scss

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,24 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-import SignIn from "./components/SignIn/SignIn";
+import SignInPage from "./pages/SignInPage/SignInPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage/HomePage";
 import NotFound from "./pages/NotFound/NotFound";
 
-const App = () => {
+export default function App() {
   return (
     <div>
       <Router>
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
     </div>
   );
-};
-
-export default App;
+}
